a2b5bd42de4c95f38992ecf9f7caec4282b897a1
WonderJolie <wonderjolie@163.com>
2015-08-08 21:35:13 +0800

    01

---
+++ 01_init/script.js
@@ -0,0 +1,8 @@
+onload=function(){
+var c=document.getElementById('canvas');
+c.width=window.innerWidth;
+c.height=window.innerHeight-3;
+var gl=c.getContext('webgl')||c.getContext('experimental-webgl');
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+}
